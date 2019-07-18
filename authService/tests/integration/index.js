bab17a04222c9d567d886b30bbcc408d4866eca8
Yogesh <yogesh.jadhav8177@gmail.com>
2019-07-18 21:22:08 +0530

    changing test sequence so as to check if there is any timing issue

--- authService/tests/integration/index.js
+++ authService/tests/integration/index.js
@@ -11,20 +11,6 @@ before(function(done) {
 
 describe(`Auth api tests`, () => {
 
-    it(`Should fetch 2 seeded users`, done => {
-        let limit = 2, page = 0;
-        request(app)
-        .get('/fetch-users')
-        .query({limit, page})
-        .then(response => {
-            expect(response.statusCode).to.equal(200);
-            expect(response.body.usersList).to.be.an('array');
-            expect(response.body.usersList).to.have.length(limit);
-            done();
-        })
-        .catch(error => done(error));
-    });
-
     it(`Should signup a new user`, done => {
         let user = {
             id: 3,
@@ -39,6 +25,20 @@ describe(`Auth api tests`, () => {
         .then(response => {
             expect(response.statusCode).to.equal(200);
             expect(response.body.message).to.equal('User sign up successful');
+            done();
+        })
+        .catch(error => done(error));
+    });
+
+    it(`Should fetch 2 seeded users`, done => {
+        let limit = 2, page = 0;
+        request(app)
+        .get('/fetch-users')
+        .query({limit, page})
+        .then(response => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.body.usersList).to.be.an('array');
+            expect(response.body.usersList).to.have.length(limit);
             done();
         })
         .catch(error => done(error));
